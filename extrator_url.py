8ade2ea1144649b96031e3bd9b42a5e81677e4ab
Bruno Godoi <bruno.dowload102@gmail.com>
2023-02-08 23:25:15 -0300

    43%

--- extrator_url.py
+++ extrator_url.py
@@ -1,10 +1,40 @@
 class ExtratorURL:
     def __init__(self,url):
         self.url =self.sanitiza_url(url)
+        self.valida_url()
 
     def sanitiza_url(self, url):
         return url.strip()
 
+    def valida_url(self):
+        if self.url == "":
+            raise ValueError("A URL está vazia")
 
-extator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
-valor_quantidade = extator_url.get_valor_parametro("")
+    def get_url_base(self):
+        indice_interrogacao = self.url.find("?")
+        url_base = self.url[0:indice_interrogacao]
+        return url_base
+
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find("?")
+        url_paramentros = self.url[indice_interrogacao+1:]
+        return url_paramentros
+
+    def get_valor_parametro(self, parametros_busca):
+        indice_parametro = self.get_url_parametros().find(parametros_busca)
+        indice_valor = indice_parametro + len(parametros_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
+        return valor
+
+
+
+
+
+
+#extator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
+extator_url = ExtratorURL("")
+#valor_quantidade = extator_url.get_valor_parametro("")
